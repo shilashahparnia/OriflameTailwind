b2708d1aaf42a3dfa77e5f9fc97b9cb4fe62aa34
shila.shahparnia <shila.shahparnia@gmail.com>
2023-04-10 17:48:02 +0200

    menu of 2xl xl sizes has been added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ var swiper = new Swiper(".myShila", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper = new Swiper(".myShahpar", {
+  slidesPerView: 6,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+  },
+});
