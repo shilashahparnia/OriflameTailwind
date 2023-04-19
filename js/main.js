188a578d1b3d5d8c1afd1b09c52d90b85a0eca38
shila.shahparnia <shila.shahparnia@gmail.com>
2023-04-19 19:32:32 +0200

    zoomIn and zoomOut have been added to images

--- js/main.js
+++ js/main.js
@@ -50,7 +50,6 @@ user.addEventListener("mouseleave", function () {
 let bars = document.querySelector("i.fa-bars");
 bars.addEventListener("mouseover", function () {
   bars.setAttribute("class", "fa-solid fa-bars fa-2xl");
-  
 });
 bars.addEventListener("mouseleave", function () {
   bars.setAttribute("class", "fa-solid fa-bars fa-xl");
@@ -59,7 +58,6 @@ bars.addEventListener("mouseleave", function () {
 let barsDark = document.querySelector("i.fa-bars#dark");
 barsDark.addEventListener("mouseover", function () {
   barsDark.setAttribute("class", "fa-solid fa-bars fa-2xl");
-  
 });
 barsDark.addEventListener("mouseleave", function () {
   barsDark.setAttribute("class", "fa-solid fa-bars fa-xl");
@@ -104,7 +102,97 @@ item3.addEventListener("click", function () {
   }
 });
 
-let darkMode=document.querySelector("button#btn")
-darkMode.addEventListener("click", function(){
-  document.querySelector("html").classList.toggle("dark")
-})
+let darkMode = document.querySelector("button#btn");
+darkMode.addEventListener("click", function () {
+  document.querySelector("html").classList.toggle("dark");
+});
+
+let zoomIn = document.querySelector("a#zoom");
+zoomIn.addEventListener("mouseover", function () {
+  zoomIn.style.height = "350px";
+});
+let zoomOut = document.querySelector("a#zoom");
+zoomOut.addEventListener("mouseleave", function () {
+  zoomOut.style.height = "210px";
+});
+
+let zoomIn2 = document.querySelector("a#zoom2");
+zoomIn2.addEventListener("mouseover", function () {
+  zoomIn2.style.height = "350px";
+});
+let zoomOut2 = document.querySelector("a#zoom2");
+zoomOut2.addEventListener("mouseleave", function () {
+  zoomOut2.style.height = "210px";
+});
+
+let zoomIn3 = document.querySelector("a#zoom3");
+zoomIn3.addEventListener("mouseover", function () {
+  zoomIn3.style.height = "330px";
+});
+let zoomOut3 = document.querySelector("a#zoom3");
+zoomOut3.addEventListener("mouseleave", function () {
+  zoomOut3.style.height = "210px";
+});
+
+let zoomIn4 = document.querySelector("a#zoom4");
+zoomIn4.addEventListener("mouseover", function () {
+  zoomIn4.style.height = "330px";
+});
+let zoomOut4 = document.querySelector("a#zoom4");
+zoomOut4.addEventListener("mouseleave", function () {
+  zoomOut4.style.height = "210px";
+});
+
+let zoomIn5 = document.querySelector("a#zoom5");
+zoomIn5.addEventListener("mouseover", function () {
+  zoomIn5.style.height = "330px";
+});
+let zoomOut5 = document.querySelector("a#zoom5");
+zoomOut5.addEventListener("mouseleave", function () {
+  zoomOut5.style.height = "210px";
+});
+
+let zoomIn6 = document.querySelector("a#zoom6");
+zoomIn6.addEventListener("mouseover", function () {
+  zoomIn6.style.height = "330px";
+});
+let zoomOut6 = document.querySelector("a#zoom6");
+zoomOut6.addEventListener("mouseleave", function () {
+  zoomOut6.style.height = "210px";
+});
+
+let zoomIn7 = document.querySelector("a#zoom7");
+zoomIn7.addEventListener("mouseover", function () {
+  zoomIn7.style.height = "330px";
+});
+let zoomOut7 = document.querySelector("a#zoom7");
+zoomOut7.addEventListener("mouseleave", function () {
+  zoomOut7.style.height = "210px";
+});
+
+let zoomIn8 = document.querySelector("a#zoom8");
+zoomIn8.addEventListener("mouseover", function () {
+  zoomIn8.style.height = "330px";
+});
+let zoomOut8 = document.querySelector("a#zoom8");
+zoomOut8.addEventListener("mouseleave", function () {
+  zoomOut8.style.height = "210px";
+});
+
+let zoomIn9 = document.querySelector("a#zoom9");
+zoomIn9.addEventListener("mouseover", function () {
+  zoomIn9.style.height = "330px";
+});
+let zoomOut9 = document.querySelector("a#zoom9");
+zoomOut9.addEventListener("mouseleave", function () {
+  zoomOut9.style.height = "210px";
+});
+
+let zoomIn10 = document.querySelector("a#zoom10");
+zoomIn10.addEventListener("mouseover", function () {
+  zoomIn10.style.height = "330px";
+});
+let zoomOut10 = document.querySelector("a#zoom10");
+zoomOut10.addEventListener("mouseleave", function () {
+  zoomOut10.style.height = "210px";
+});
